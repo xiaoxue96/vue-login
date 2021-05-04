d2a2c2a884e3a54021be7480c8ed9c95a29782a2
sybil3904 <sybil3904@163.com>
2021-05-04 14:04:17 +0800

    deploy

--- src/services/user.js
+++
@@ -1,9 +0,0 @@
-const users = [{ username: 'admin', password: 'admin' }];
-
-export default {
-  login: ({ username, password }) => {
-    return users.some(
-      (user) => user.username === username && user.password === password
-    );
-  },
-};
--- vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  outputDir: './docs',
-  assetsDir: 'static',
-  productionSourceMap: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '' : '/vue-login/',
-};
